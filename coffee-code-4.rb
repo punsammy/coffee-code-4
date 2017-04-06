1a73dd18a39594e588995eecc17e96f7ec53dd85
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-06 09:54:57 -0400

    update Combining Array

--- coffee-code-4.rb
+++ coffee-code-4.rb
@@ -20,7 +20,13 @@ numbers.reduce(:*)
 #Combining Arrays
 numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['spades', 'hearts', 'diamonds', 'clubs']
+cards = []
 
-cards = Hash[numbers.zip(suits.map {|i| i.split /, /})]
+suits.each do |suit|
+  numbers.each do |number|
+    cards << [suit, number]
+  end
+end
 
-numbers.product(suits)
+
+# numbers.product(suits)
